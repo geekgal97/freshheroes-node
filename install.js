f5a9a9f2e2ee3a6c2d2099400a533d8de36ecbba
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-05 11:29:14 +0200

    :sparkles: add more fake data and datamodels

--- install.js
+++ install.js
@@ -2,59 +2,117 @@
  * Initializes demo users in DB
  */
 
+const faker = require('faker');
 const db = require('mongoose').connect('mongodb://localhost/freshheroes');
 const User = require('./models/user');
+const Vacancy = require('./models/vacancy');
+const Company = require('./models/company');
 
-const demoUsers = [
-  {
-    email: 'pim@lifely.nl',
-    password: 'test',
-    type: 'company',
-    birthday: randomBirthday(),
-    firstname: 'Pim',
-    lastname: 'Verlaan',
-    admin: true
-  },
-  {
-    email: 'bryan@lifely.nl',
-    password: 'test',
-    type: 'student',
-    birthday: randomBirthday(),
-    firstname: 'Bryan',
-    lastname: 'te Beek'
-  },
-  {
-    email: 'redbull@lifely.nl',
-    password: 'test',
-    type: 'company',
-    birthday: randomBirthday(),
-    firstname: 'Redbull',
-    lastname: 'Owner'
-  },
-  {
-    email: 'rabobank@lifely.nl',
-    password: 'test',
-    type: 'company',
-    birthday: randomBirthday(),
-    firstname: 'Bryan',
-    lastname: 'te Beek'
-  },
-  {
-    email: 'ddd@lifely.nl',
-    password: 'test',
-    type: 'company',
-    birthday: randomBirthday(),
-    firstname: 'Bryan',
-    lastname: 'te Beek'
-  }
-];
+// Users
+// -----------------------------------------------------------------------------
+const pim = new User({
+  email: 'pim@lifely.nl',
+  password: 'test',
+  type: 'company',
+  birthday: randomBirthday(),
+  firstname: 'Pim',
+  lastname: 'Verlaan',
+  admin: true
+});
 
-demoUsers.forEach((user, i) => {
-  new User(user).save();
+const bryan = new User({
+  email: 'bryan@lifely.nl',
+  password: 'test',
+  type: 'student',
+  birthday: randomBirthday(),
+  firstname: 'Bryan',
+  lastname: 'te Beek'
+});
 
-  if (i === demoUsers.length - 1) {
-    db.connection.close();
-  }
+const redbull = new User({
+  email: 'redbull@lifely.nl',
+  password: 'test',
+  type: 'company',
+  birthday: randomBirthday(),
+  firstname: 'Redbull',
+  lastname: 'Owner'
+});
+
+const rabobank = new User({
+  email: 'rabobank@lifely.nl',
+  password: 'test',
+  type: 'company',
+  birthday: randomBirthday(),
+  firstname: 'Bryan',
+  lastname: 'te Beek'
+});
+
+const ddd = new User({
+  email: 'ddd@lifely.nl',
+  password: 'test',
+  type: 'company',
+  birthday: randomBirthday(),
+  firstname: 'Bryan',
+  lastname: 'te Beek'
+});
+
+// Company
+// -----------------------------------------------------------------------------
+const lifely = new Company({
+  name: 'Lifely',
+  intro: 'Lifely, architect in online. Een digital agency met een rebelse balans tussen concept, design, content en techniek.',
+  description: faker.paragraph(5),
+  type: 'startup',
+  amountOfEmployees: 10,
+  status: 'active',
+  website: 'lifely.nl',
+  social: {
+    facebook: 'facebook.com/lifelynl',
+    twitter: 'twitter.com/lifelynl',
+    linkedin: 'linkedin.com/lifely'
+  },
+  userId: pim._id
+});
+
+// Vacancies
+// -----------------------------------------------------------------------------
+const vacancy1 = new Vacancy({
+  companyId: lifely._id,
+  category: 'development',
+  address: {
+    street: 'Zekeringstraat 23B',
+    zipcode: '1014 BM',
+    city: 'Amsterdam'
+  },
+  deadline: new Date(),
+  name: 'Developer (with Report)'
+});
+
+const vacancy2 = new Vacancy({
+  companyId: lifely._id,
+  category: 'development',
+  address: {
+    street: 'Zekeringstraat 23B',
+    zipcode: '1014 BM',
+    city: 'Amsterdam'
+  },
+  deadline: new Date(),
+  name: 'Developer (with Report)'
+});
+
+Promise.all([
+  lifely.save(),
+  bryan.save(),
+  redbull.save(),
+  rabobank.save(),
+  ddd.save(),
+
+  vacancy1.save()
+])
+.then(() => db.connection.close())
+.catch(() => {
+  console.log('\n\nRecords bestaan al\n\n');
+  db.connection.close();
 });
 
 function randomBirthday() {
