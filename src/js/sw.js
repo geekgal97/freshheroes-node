56728478ca944e3a1c7b907e2196518266e6d846
Danny de Vries <vriesdan@gmail.com>
2017-04-06 13:35:57 +0200

    :bulb: Comments about sw

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,1 +1,10 @@
-console.log('This is a Service Worker');
+// Replace registered Service Worker
+self.addEventListener('install', event =>
+  event.waitUntil(self.skipWaiting()));
+
+// Hijack fetch return custom response
+self.addEventListener('fetch', event => {
+  event.respondWidth(new Response('Pirate direct'));
+});
+
+console.log('It has works');
