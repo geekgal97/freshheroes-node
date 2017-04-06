2ef819c29ae95ba073ed06c51d177e53d0e95289
Danny de Vries <vriesdan@gmail.com>
2017-04-06 15:20:29 +0200

    :bento: precache assets on install

--- src/js/sw.js
+++ src/js/sw.js
@@ -3,8 +3,15 @@
 self.addEventListener('install', event => event.waitUntil(
   // Cache object and Cache name
   caches.open('freshheroes-node')
-    // Add folder to cache object
-    .then(cache => cache.add('/'))
+    // Add files to cache object
+    .then(cache => cache.addAll([
+      '/',
+      'images/*',
+      //CSS files
+      'account.css', 'admin.css', 'home.css','other.css','style.css',
+      //JS files
+      'admin.js', 'home.js', 'index.js'
+    ]))
     //Become the active service worker
     //Prevents the browser from terminating the service worker
     .then(self.skipWaiting())
@@ -13,12 +20,15 @@ self.addEventListener('install', event => event.waitUntil(
 // Hijack fetch return custom response
 // Listen for fetch event
 self.addEventListener('fetch', event => {
-  //Generate custom response-generating code
-  event.respondWith(new Response('hijacked'));
+  // Use offline page on failed fetch
+  event.respondWith(
+    fetch(event.request)
+      .catch(err => fetchOfflinePage())
+  );
 });
 
-// Use offline page if fetch fails
-function fetchOfflienPage() {
+// Use offline page on failed fetch
+function fetchOfflinePage() {
   return.caches.open('freshheroes-node')
     .then(cache => cache.match('/'));
 }
