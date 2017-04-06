da79f7ce389953ca99902d07abcee86a38812324
Danny de Vries <vriesdan@gmail.com>
2017-04-06 15:09:26 +0200

    :sparkles: cache page on install

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,13 +1,18 @@
 // Skip waiting on install
 // Listen for install event
-self.addEventListener('install', event =>
-  //Prevents the browser from terminating the service worker
-  //Become the active service worker
-  event.waitUntil(self.skipWaiting()));
+self.addEventListener('install', event => event.waitUntil(
+  // Cache object and Cache name
+  caches.open('freshheroes-node')
+    // Add to cache object
+    .then(cache => cache.add('/'))
+    //Become the active service worker
+    //Prevents the browser from terminating the service worker
+    .then(self.skipWaiting())
+));
 
 // Hijack fetch return custom response
 //Listen for fetch event
 self.addEventListener('fetch', event => {
-  //Generate custom resposne-generating code
+  //Generate custom response-generating code
   event.respondWith(new Response('hijacked'));
 });
